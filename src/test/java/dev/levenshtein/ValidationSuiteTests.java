eed3813e8a04716999c20e831afd75a9a6088ebd
Mickaël Seffar <mickael.seffar@gmail.com>
2024-10-25 16:14:31 +0200

    TP01 - JUnit

---
+++ src/test/java/dev/levenshtein/ValidationSuiteTests.java
@@ -0,0 +1,11 @@
+package dev.levenshtein;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.runner.RunWith;
+
+@IncludeTags("ValidationEntree")
+@RunWith(JUnitPlatform.class)
+public class ValidationSuiteTests {
+}
